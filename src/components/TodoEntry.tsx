8cb87c381bba2e6a7d2aa29724195bb1dd147c5c
Lixian Dai <lixian.dai@aptos.com>
2023-03-08 10:40:11 -0500

    buttons with css

--- src/components/TodoEntry.tsx
+++ src/components/TodoEntry.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Task } from "../App";
+import Toolbar from "./Toolbar";
 
 type TodoEntryProps = {
   task: Task;
@@ -9,15 +10,22 @@ export const TodoEntry: React.FC<TodoEntryProps> = (props) => {
   const { task, toggleTodoStatus } = props;
   return (
     <>
-      <span
-        onClick={()=>toggleTodoStatus(task.id)}
-        style={{
-          cursor: "pointer",
-          textDecoration: task.completed ? "line-through" : "none",
-        }}
-      >
-        {task.text}
-      </span>
+      <div className="todoEntry row">
+        <div className="col">
+          <span
+            onClick={() => toggleTodoStatus(task.id)}
+            style={{
+              cursor: "pointer",
+              textDecoration: task.completed ? "line-through" : "none",
+            }}
+          >
+            {task.text}
+          </span>
+        </div>
+        <div className="col-4">
+          <Toolbar></Toolbar>
+        </div>
+      </div>
     </>
   );
 };
